6dfad6b91a6b1b9585603fa052a058310a69a4aa
An-Mao <lxxl@live.cn>
2026-07-03 15:11:12 +0800

    up to 25.0.2

---
+++ src/main/java/dev/anye/mc/graaljs/GraalJS.java
@@ -0,0 +1,21 @@
+package dev.anye.mc.graaljs;
+
+import com.mojang.logging.LogUtils;
+
+import dev.anye.mc.graaljs.javascript.EasyJS;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod(GraalJS.MOD_ID)
+public class GraalJS
+{
+    public  static final String MOD_ID = "graaljs";
+    private static final Logger LOGGER = LogUtils.getLogger();
+    public GraalJS()
+    {
+        LOGGER.info("[GraalJS Test] 1 + 1 = {}",EasyJS.NotSafe().runCode("1+1"));
+    }
+
+}
