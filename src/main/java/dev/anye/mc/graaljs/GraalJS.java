76290fa6172daa5cb598c2ca884c34c9e8d464a5
An-Mao <lxxl@live.cn>
2026-07-05 16:17:49 +0800

    opt

--- src/main/java/dev/anye/mc/graaljs/GraalJS.java
+++ src/main/java/dev/anye/mc/graaljs/GraalJS.java
@@ -3,8 +3,6 @@ package dev.anye.mc.graaljs;
 import com.mojang.logging.LogUtils;
 
 import dev.anye.mc.graaljs.javascript.EasyJS;
-import net.neoforged.bus.api.IEventBus;
-import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import org.slf4j.Logger;
 
